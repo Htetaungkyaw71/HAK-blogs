a9aea80accddff42c31e4eb879c20ce0568f4553
htetaungkyaw <htetaung200071@gmail.com>
2021-08-19 23:17:50 +0630

    change edit

--- main.py
+++ main.py
@@ -231,7 +231,6 @@ def edit_post(post_id):
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
